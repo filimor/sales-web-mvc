45cc680e734eceed82a5fcad00c3d379995cf250
Filipe Moreira <filimor@posteo.com.br>
2019-12-29 18:45:20 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Author"] = "Filipe Moreira";
+            ViewData["Email"] = "filimor@posteo.com.br";
 
             return View();
         }
